35249f49eb750d71bf48281be312c96e09c0ec3e
MinariYoshinari <mii.informationsystems@gmail.com>
2016-10-29 13:06:35 +0900

    分類器を作り直した

--- period_putter/learn.py
+++ period_putter/learn.py
@@ -1,16 +1,16 @@
 from sklearn.datasets import load_svmlight_file
-from sklearn.cross_validation import train_test_split
 from sklearn.grid_search import GridSearchCV
+from sklearn.cross_validation import train_test_split
 from sklearn.metrics import classification_report
 from sklearn.svm import SVC
 from sklearn.externals import joblib
 import numpy
 
-#grid_searchいわく，最適なパラメータは
-#SVC(C=1, cache_size=200, class_weight=None, coef0=0.0,
+#SVC(C=10, cache_size=200, class_weight=None, coef0=0.0,
 #  decision_function_shape=None, degree=3, gamma='auto', kernel='linear',
 #  max_iter=-1, probability=False, random_state=None, shrinking=True,
 #  tol=0.001, verbose=False)
+
 
 def grid_search(train_features, train_labels):
     param_grid = [
@@ -25,16 +25,17 @@ def grid_search(train_features, train_labels):
 x,y = load_svmlight_file('SC_svmlight.txt')
 x = x.toarray()
 
-#clf = SVC(C=1, cache_size=200, class_weight=None, coef0=0.0,
+#clf = SVC(C=10, cache_size=200, class_weight=None, coef0=0.0,
 #  decision_function_shape=None, degree=3, gamma='auto', kernel='linear',
 #  max_iter=-1, probability=False, random_state=None, shrinking=True,
 #  tol=0.001, verbose=False)
 
 clf = joblib.load('clf.pkl')
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.99, random_state=0)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+#grid_search(x_train, y_train)
 
-#clf.fit(x_train, y_train)
+clf.fit(x_train, y_train)
 print(clf.score(x_test, y_test))
 
-#joblib.dump(clf, 'clf.pkl')
+joblib.dump(clf, 'clf.pkl')
--- period_putter/period_putter.py
+++ period_putter/period_putter.py
@@ -17,6 +17,47 @@ class Period_putter():
 
 	def __init__(self):
 		self.clf = joblib.load('clf.pkl')
+		self.all_pos_list = ['名詞', '名詞接尾辞', '冠名詞', '英語接尾辞', '動詞語幹', '動詞活用語尾', '動詞接尾辞', '冠動詞', '補助名詞', '形容詞語幹', '形容詞接尾辞', '冠形容詞', '連体詞', '連用詞', '接続詞', '独立詞', '接続接尾辞', '判定詞', '格助詞', '引用助詞', '連用助詞', '終助詞', '間投詞', '括弧', '句点', '読点', 'Symbol', 'Year', 'Month', 'Day', 'YearMonth', 'MonthDay', 'Hour', 'Minute', 'Second', 'HourMinute', 'MinuteSecond', 'PreHour', 'PostHour', 'Number', '助数詞', '助助数詞', '冠数詞', 'Alphabet', 'Kana', 'Katakana', 'Kanji', 'Roman', 'Undef']
+		#解析した文のベクトルを入れる配列
+		self.vec = [0] * 200
+
+	#ベクトルを2値ベクトルに変換
+	def to_binary_vec(self, vec_dec):
+		for (i,item) in enumerate(vec_dec):
+			for j in range(50):
+				if j == 49:
+					self.vec[i] = (int(item == -1)
+				else:
+					print(int(item == i), end="")
+
+
+	#形態素解析の結果から分類器用のベクトルをつくり，分類する
+	def make_vec(self, result_json):
+		#print(result_json)
+		sentences = json.loads(result_json)["word_list"]
+		pos_list = []
+
+		#句点のない文章だからここでは必ず1文になるはずだけど，一応文分割処理入れておく
+		for sentence in sentences:
+			for pos in sentence:
+				pos_list.append(pos[0])
+
+		n = len(pos_list)
+		#句点の入る位置と思われる場所を基準として[2つ前，1つ前，1つ後，2つ後]
+		vec_dec = [-1, -1, -1, -1]
+		for i in range(0, n-1):
+			vec_dec[0] = vec_dec[1]
+			vec_dec[1] = pos_list[i] if vec_dec[2] == -1 else vec_dec[2]
+			vec_dec[2] = pos_list[i+1] if vec_dec[3] == -1 else vec_dec[3]
+			vec_dec[3] = pos_list[i+2] if i < n-2 else -1
+			print(vec_dec)
+			self.to_binary_vec(vec_dec)
+
+			#self.clf.predict(#予測したいベクトルを入れる)
+
+
+		return pos_list
+
 
 	#形態素解析APIにPOSTを投げる
 	def post(self, contents):
@@ -31,16 +72,13 @@ class Period_putter():
 		with urllib.request.urlopen(request) as response:
 			response_body = response.read().decode("utf-8")
 			return response_body
-		return None 
+		return None
 
 	#ピリオド挿入プロセス全体
 	def put_period(self, plane_string):
 		print(plane_string)
-		print(self.post(plane_string))
-
-
-
-
+		result_json = self.post(plane_string)
+		vec_binary = self.make_vec(result_json)
 		return
 
 pp = Period_putter()
