3a2e715a06586b7989f421d88f6c53e3e9c8f190
Hiroaki Goto <goto.inct@gmail.com>
2016-10-29 18:27:28 +0900

    db/schema.rb が更新されていなかったので修正。

--- smartwalk_server/db/schema.rb
+++ smartwalk_server/db/schema.rb
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20161029081806) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["token"], name: "index_registration_tokens_on_token", unique: true
+    t.index ["user_id"], name: "index_registration_tokens_on_user_id"
   end
 
   create_table "rooms", force: :cascade do |t|
