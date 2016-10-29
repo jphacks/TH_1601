0bb20e6dc512d8b693665dd2819ebe26d792d960
Hiroaki Goto <goto.inct@gmail.com>
2016-10-29 18:19:03 +0900

    クラスメソッドになっていなかったのを修正。

--- smartwalk_server/app/models/registration_token.rb
+++ smartwalk_server/app/models/registration_token.rb
@@ -3,14 +3,14 @@ class RegistrationToken < ApplicationRecord
   validates :token, presence: true
   validates :token, uniqueness: true
 
-  def generate_random_string
+  def self.generate_random_string
     domain =  [('a'..'z'), ('A'..'Z'), ('0'..'9')].map{|i| i.to_a}.flatten
     (0...20).map{domain.to_a[rand(domain.length)] }.join
   end
 
-  def generate_token_for(user)
+  def self.generate_token_for(user)
     token_obj = RegistrationToken.new
-    token_obj.token = generate_random_string
+    token_obj.token = RegistrationToken.generate_random_string
     token_obj.user = user
     token_obj.save!
     token_obj.token
