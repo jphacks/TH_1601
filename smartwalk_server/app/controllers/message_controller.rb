271b9138436b5f52b877ea41b4b4cd9c05c26aa7
Hiroaki Goto <goto.inct@gmail.com>
2016-10-30 06:45:10 +0900

    多分表示名で送れるようになった。

--- smartwalk_server/app/controllers/message_controller.rb
+++ smartwalk_server/app/controllers/message_controller.rb
@@ -2,10 +2,17 @@ class MessageController < ApplicationController
   def push
     body = request.body.read
     json = JSON.parse(body)
-    sender = json['sender']
-    display_name = json['displayName']
+    sender_user_id = json['sender']
+    display_name = json['display_name']
     receiver = json['receiver']
     unless receiver then
+      begin
+        sender = User.find_by(user_id: sender_user_id)
+        receiver = sender.friends.where(display_name: display_name).take.user_id
+      rescue
+        logger.debug("Cannot determine receiver")
+        return head :bad_request
+      end
     end
     message = {
       type: 'text',
