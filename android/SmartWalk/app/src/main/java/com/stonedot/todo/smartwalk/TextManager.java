cdfc1f95a75f9d60ce3074ba036768744cb7b4bc
komatsu <a1602015@sendai-nct.jp>
2016-11-06 12:07:51 +0900

    pictograph更新

--- android/SmartWalk/app/src/main/java/com/stonedot/todo/smartwalk/TextManager.java
+++ android/SmartWalk/app/src/main/java/com/stonedot/todo/smartwalk/TextManager.java
@@ -13,6 +13,7 @@ public class TextManager {
 
     public static String extractSpeakableChars(String text) {
         StringBuilder sb = new StringBuilder();
+        Log.d("TextManager", text);
         for(int i = 0; i < text.length(); i++ ){
             //絵文字じゃなかったら追加
             if( Character.UnicodeBlock.of( text.charAt( i )) != Character.UnicodeBlock.MISCELLANEOUS_SYMBOLS_AND_PICTOGRAPHS){
