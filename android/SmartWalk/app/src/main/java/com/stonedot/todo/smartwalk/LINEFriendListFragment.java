d9bf806c1547dfbd81955f32dd4ddd1c6462675b
Hiroaki Goto <goto.inct@gmail.com>
2016-11-06 19:12:01 +0900

    friendList取得メソッドを実装。

--- android/SmartWalk/app/src/main/java/com/stonedot/todo/smartwalk/LINEFriendListFragment.java
+++ android/SmartWalk/app/src/main/java/com/stonedot/todo/smartwalk/LINEFriendListFragment.java
@@ -5,13 +5,21 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class LINEFriendListFragment extends Fragment {
 
@@ -48,13 +56,58 @@ public class LINEFriendListFragment extends Fragment {
         mListView.setAdapter(adapter);
     }
 
-    private ArrayList<String> friendList() {
+    class ItemPair {
+        public String mImageUrl;
+        public String mDisplayName;
+        public ItemPair(String displayName, String imageUrl) {
+            mDisplayName = displayName;
+            mImageUrl = imageUrl;
+        }
+    }
+
+    interface ItemAdder {
+        public void ItemAdded(ItemPair pair);
+    }
+
+    private static final String FriendListEndpoint = "https://smartwalk.stonedot.com/users/friend_list";
+    private void friendList(ItemAdder callback) {
         ArrayList<String> resultList = new ArrayList<String>();
         resultList.add("Pen");
         resultList.add("PineApple");
         resultList.add("Apple");
         resultList.add("Pen");
-        return resultList;
+        HashMap<String, String> sendData = new HashMap<String, String>() {
+            {
+                put("mid", UserDataStorage.getLineMid(getActivity()));
+            }
+        };
+        try {
+            URL url = new URL(FriendListEndpoint);
+            HttpJSONClient client = new HttpJSONClient(url, sendData);
+            client.post(new HttpJSONClient.Responded() {
+                @Override
+                public void responded(int code, String statusMessage, String content) {
+                    try {
+                        JSONObject json = new JSONObject(content);
+                        JSONArray friends = json.getJSONArray("friend_list");
+                        for(int i = 0; i < friends.length(); i++) {
+                            JSONObject friend = friends.getJSONObject(i);
+                            if(callback != null) {
+                                String displayName = friend.getString("display_name");
+                                String pictureUrl = friend.getString("picture_url");
+                                callback.ItemAdded(new ItemPair(displayName, pictureUrl));
+                            }
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        Log.d("LineFriendList", "JSONの解析に失敗。");
+                    }
+                }
+            });
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            Log.d("LineFriendList", "フレンドリストの取得に失敗。");
+        }
     }
 
     @Override
